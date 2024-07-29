a6840219c9b4dee090d37b5ae13f74fd919657c4
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2024-07-29 20:06:34 +0200

    updated install script

--- install_llama.sh
+++ install_llama.sh
@@ -1,4 +1,13 @@
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --upgrade --force-reinstall llama-cpp-python --no-cache-dir
+pip install python-dateutil
+pip insatll requests
+pip install ninja
+pip install eventlet
+pip install python-socketio
 
+CMAKE_ARGS="-DGGML_CUDA=on" FORCE_CMAKE=1 pip install \
+  --upgrade \
+  --force-reinstall \
+  --no-cache-dir \
+  llama-cpp-python
 
 
